db65c0920e8e529b475c7d47bcdb162bead6dd96
Nino Zivan <nino.zivan@nextblink.com>
2017-02-20 15:28:47 +0100

    After merging with V-006
    Inputed some changes on data and binding

--- modules/home/home-controller.js
+++ modules/home/home-controller.js
@@ -11,8 +11,8 @@ angular.module('Home')
               { id: "AL", title: "Albania" },
               { id: "AM", title: "Armenia" },
               { id: "AZ", title: "Azerbaijan" },
-              { id: "BA", title: "Bosnia and Herzegovina" },
               { id: "BY", title: "Belarus" },
+              { id: "BA", title: "Bosnia and Herzegovina" },
               { id: "MK", title: "FYR Macedonia" },
               { id: "GE", title: "Georgia" },
               { id: "KZ", title: "Kazakhstan" },
@@ -24,8 +24,8 @@ angular.module('Home')
               { id: "TJ", title: "Tajikistan" },
               { id: "TR", title: "Turkey" },
               { id: "TM", title: "Turkmenistan" },
-              { id: "UZ", title: "Uzbekistan" },
-                { id: "UA", title: "Ukraine" }
+                { id: "UA", title: "Ukraine" },
+                { id: "UZ", title: "Uzbekistan" }
             ];
             /* #region Variables for application */
             $scope.ctrlVars = {
@@ -211,6 +211,9 @@ angular.module('Home')
                     $scope.ctrlVars.visibleStates.isCountryVisible = !$scope.ctrlVars.visibleStates.isCountryVisible;
                     $scope.ctrlVars.visibleStates.isOpenCountryBACdata_lg = false;
                 } else if (callerName == 'countryLearnMore_lg') {
+                    if ($scope.ctrlVars.visibleStates.isOpenCountryBACdata_lg == false) {
+                        $scope.changeCountryBACData(0);
+                    }
                     $scope.ctrlVars.visibleStates.isOpenCountryBACdata_lg = !$scope.ctrlVars.visibleStates.isOpenCountryBACdata_lg;
                 } else if (callerName == 'bacContentMoreLess') {
                     if ($scope.ctrlVars.dataBindBAC.isTextExpanded != undefined && $scope.ctrlVars.dataBindBAC.isTextExpanded != null) {
@@ -250,6 +253,9 @@ angular.module('Home')
             $(document).on('click', '#cid-footer-navigations-xs .c-clickable-item', function (e) {
                 e.stopPropagation();
             });
+            $(document).on('click', '#cid-okw-sto-prop .c-sto-prop', function (e) {
+                e.stopPropagation();
+            });
             /* #===================================================================== Countries Map related code =======# */
             $scope.countryClickedFunc = function (callerName, countryIndex) {
                 if (callerName == 'countriesFromBurger') {
@@ -259,14 +265,18 @@ angular.module('Home')
                     var countryId = undpWorldMap.dataProvider.areas[countryIndex].id;
                     var newCountryObject = undpWorldMap.getObjectById(countryId);
                     undpWorldMap.clickMapObject(newCountryObject);
+                    //$scope.ctrlVars.selectedCountry = siteData.countries[countryIndex];
+                    //$scope.ctrlVars.visibleStates.isCountryVisible = true;
+                    //$scope.ctrlVars.selectedCountryIndex = countryIndex;
                     $timeout(function () {
                         scrollToAnchor('mapdiv');
                     }, 600);
+                } else {
+                    $scope.ctrlVars.selectedCountry = siteData.countries[countryIndex];
+                    $scope.ctrlVars.visibleStates.isCountryVisible = true;
+                    $scope.ctrlVars.selectedCountryIndex = countryIndex;
+                    $scope.changeCountryBACData(0);
                 };
-                $scope.ctrlVars.selectedCountry = siteData.countries[countryIndex];
-                $scope.ctrlVars.visibleStates.isCountryVisible = true;
-                $scope.ctrlVars.selectedCountryIndex = countryIndex;
-                $scope.changeCountryBACData(0);
             }
             $scope.changeCountryBACData = function (countryBACbttnIndex) {
                 for (var i = 0; i < $scope.ctrlVars.enumBAC.length; i++) {
